dcc7c33715425249709a9f60fff98787e699ceb5
abhishekkhosla <46638068+abhishekkhosla@users.noreply.github.com>
2023-10-06 18:17:43 +0000

    Variable And DataTypes

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,7 @@
+const accountId = 1145;
+let accountEmail = 'aks@gmail.com';
+var accountPwd = '123456';
+console.table([accountEmail,accountPwd]);
+/*
+Prefer not to user Var bcos of Block Scope and Functional Scope
+*/
